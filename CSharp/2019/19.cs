78555399011f24ea47b8f717d19e12b088c2a6ed
sawyersteven <sawyerstevenk@gmail.com>
2021-03-24 15:40:09 -0400

    2019 Day 19

--- CSharp/2019/19.cs
+++ CSharp/2019/19.cs
@@ -6,6 +6,7 @@ namespace Advent2019
     public class Challenge19 : Challenge
     {
         private const int gridSize = 50;
+        private const int shipSize = 100;
 
         public override object Task1()
         {
@@ -26,7 +27,36 @@ namespace Advent2019
 
         public override object Task2()
         {
-            return null;
+            IntCode.Emulator ICE = new IntCode.Emulator(input[0]) { Verbose = false };
+
+            int y = shipSize;
+            int x = 0;
+
+            bool changes = true;
+            while (changes)
+            {
+                changes = false;
+                // move down until top-right in field
+                while (true)
+                {
+                    ICE.Reboot();
+                    ICE.QueueInput(x + shipSize - 1, y);
+                    if (ICE.Run().Item2 == 1) break;
+                    y++;
+                    changes = true;
+                }
+
+                // move right until bottom-left in field
+                while (true)
+                {
+                    ICE.Reboot();
+                    ICE.QueueInput(x, y + shipSize - 1);
+                    if (ICE.Run().Item2 == 1) break;
+                    x++;
+                    changes = true;
+                }
+            }
+            return (x * 10000) + y;
         }
     }
 }
