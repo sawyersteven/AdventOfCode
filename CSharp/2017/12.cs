66f2f7a0b9e518ed2ac0f4fc63637ee64f9a00ca
sawyersteven <sawyerstevenk@gmail.com>
2021-04-04 22:43:38 -0400

    2017 day 12, 13

--- CSharp/2017/12.cs
+++ CSharp/2017/12.cs
@@ -1,17 +1,55 @@
+using System;
+using System.Collections.Generic;
 using AdventOfCode;
 
 namespace Advent2017
 {
     public class Challenge12 : Challenge
     {
+        Dictionary<string, string[]> programs;
         public override object Task1()
         {
-            return null;
+            programs = new Dictionary<string, string[]>();
+
+            foreach (string line in input)
+            {
+                programs[line.Split(' ')[0]] = line.Split("> ")[1].Split(", ");
+            }
+
+            return ProgramGroup("0").Count;
+        }
+
+        private HashSet<string> ProgramGroup(string seed)
+        {
+            HashSet<string> goodProgs = new HashSet<string>() { seed };
+            while (true)
+            {
+                int c = goodProgs.Count;
+                foreach (var kv in programs)
+                {
+                    if (goodProgs.Contains(kv.Key))
+                    {
+                        goodProgs.UnionWith(kv.Value);
+                    }
+                }
+                if (goodProgs.Count == c) break;
+            }
+            return goodProgs;
         }
 
         public override object Task2()
         {
-            return null;
+            int groupCount = 0;
+            HashSet<string> consumed = new HashSet<string>();
+
+            foreach (string k in programs.Keys)
+            {
+                if (consumed.Contains(k)) continue;
+                groupCount++;
+                consumed.UnionWith(ProgramGroup(k));
+            }
+
+            return groupCount;
         }
     }
 }
--- CSharp/2017/13.cs
+++ CSharp/2017/13.cs
@@ -1,17 +1,69 @@
+using System;
+using System.Collections.Generic;
 using AdventOfCode;
 
 namespace Advent2017
 {
     public class Challenge13 : Challenge
     {
+        SortedDictionary<int, int> ranges;
         public override object Task1()
         {
-            return null;
+            ranges = new SortedDictionary<int, int>();
+            foreach (string line in input)
+            {
+                int[] parts = Array.ConvertAll(line.Split(':'), int.Parse);
+                ranges[parts[0]] = parts[1];
+            }
+            return CalcSeverity();
+        }
+
+        private bool ScannerAtZero(int time, int range)
+        {
+            int rf = (range * 2) - 2;
+            int index = time % rf;
+            return index == 0 || (rf - index) == 0;
+        }
+
+        private int CalcSeverity()
+        {
+            int severity = 0;
+            foreach ((int time, int range) in ranges)
+            {
+                if (ScannerAtZero(time, range))
+                {
+                    severity += time * (ranges[time]);
+                }
+            }
+            return severity;
+        }
+
+        private bool CaughtByScanner(int offset)
+        {
+            foreach ((int time, int range) in ranges)
+            {
+                if (ScannerAtZero(time + offset, range))
+                {
+                    return true;
+                }
+            }
+            return false;
         }
 
         public override object Task2()
         {
-            return null;
+            for (int offset = 0; ; offset++)
+            {
+                /* have to check the zero position specificially because it
+                only raise the severity by a multiple of zero, which doesn't
+                tell if we are caught or not
+
+                There's probably a quicker, more math-y way to solve this.
+                */
+                if (ScannerAtZero(offset, ranges[0])) continue;
+
+                if (!CaughtByScanner(offset)) return offset;
+            }
         }
     }
 }
