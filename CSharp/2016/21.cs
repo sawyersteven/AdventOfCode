7794b70786011a2910a3c88a1b7a4dd71d8a4b6f
sawyersteven <sawyerstevenk@gmail.com>
2021-04-23 19:25:28 -0400

    2016 day 21,22

--- CSharp/2016/21.cs
+++ CSharp/2016/21.cs
@@ -1,3 +1,4 @@
+using System;
 using AdventOfCode;
 
 namespace Advent2016
@@ -6,12 +7,112 @@ namespace Advent2016
     {
         public override object Task1()
         {
-            return null;
+            char[] password = "abcdefgh".ToCharArray();
+            return string.Join(null, Scramble(password));
+        }
+
+        private char[] Scramble(char[] password, bool reverse = false)
+        {
+            for (int i = 0; i < input.Length; i++)
+            {
+                string line = input[i];
+                string[] parts = line.Split(' ');
+                switch (parts[0])
+                {
+                    case "swap":
+                        int x = 0;
+                        int y = 0;
+                        if (parts[1] == "position")
+                        {
+                            x = int.Parse(parts[2]);
+                            y = int.Parse(parts[5]);
+                        }
+                        else
+                        {
+                            x = Array.IndexOf(password, parts[2][0]);
+                            y = Array.IndexOf(password, parts[5][0]);
+                        }
+                        password[x] += password[y];
+                        password[y] = (char)(password[x] - password[y]);
+                        password[x] -= password[y];
+                        break;
+
+                    case "reverse":
+
+                        int start = int.Parse(parts[2]);
+                        int end = int.Parse(parts[4]);
+                        Array.Reverse(password, start, end - start + 1);
+                        break;
+
+                    case "rotate":
+                        int by = 0;
+                        if (parts[1] == "based")
+                        {
+                            by = Array.IndexOf(password, parts[6][0]);
+                            if (reverse)
+                            {
+                                if (by % 2 == 1) by -= (by / 2);
+                                else by = 5 + (by / 2);
+                                if (by == 5) by = 9;
+                                by *= -1;
+                            }
+                            else
+                            {
+                                if (by >= 4) by++;
+                                by++;
+                            }
+                        }
+                        else
+                        {
+                            by = int.Parse(parts[2]) * (parts[1] == "left" ? -1 : 1);
+                            if (reverse) by *= -1;
+                        }
+                        RotateR(password, by);
+                        break;
+
+                    case "move":
+                        int from = int.Parse(parts[2]);
+                        int to = int.Parse(parts[5]);
+
+                        if (reverse) (to, from) = (from, to);
+
+                        char m = password[from];
+                        if (from > to)
+                        {
+                            ShiftRange(password, to, from - to, 1);
+                        }
+                        else
+                        {
+                            ShiftRange(password, from + 1, to - from, -1);
+                        }
+                        password[to] = m;
+                        break;
+                }
+            }
+            return password;
+        }
+
+        private void ShiftRange<T>(T[] arr, int start, int length, int distance)
+        {
+            T[] tmp = new T[length];
+            Array.Copy(arr, start, tmp, 0, length);
+            Array.Copy(tmp, 0, arr, start + distance, tmp.Length);
+        }
+
+        private void RotateR<T>(T[] arr, int by)
+        {
+            by = (by + arr.Length + arr.Length) % arr.Length;
+            T[] buffer = new T[by];
+            Array.Copy(arr, arr.Length - by, buffer, 0, by);
+            Array.Copy(arr, 0, arr, by, arr.Length - by);
+            Array.Copy(buffer, 0, arr, 0, by);
         }
 
         public override object Task2()
         {
-            return null;
+            char[] scrambled = "fbgdceah".ToCharArray();
+            Array.Reverse(input);
+            return string.Join(null, Scramble(scrambled, true));
         }
     }
 }
--- CSharp/2016/22.cs
+++ CSharp/2016/22.cs
@@ -1,17 +1,82 @@
+using System;
+using System.Collections.Generic;
 using AdventOfCode;
+using ExtensionMethods;
+using Grids;
 
 namespace Advent2016
 {
     public class Challenge22 : Challenge
     {
+        // location: used, available
+        private Dictionary<Vector2Int, (int, int)> ParseInput()
+        {
+            Dictionary<Vector2Int, (int, int)> nodes = new Dictionary<Vector2Int, (int, int)>();
+            for (int i = 2; i < input.Length; i++)
+            {
+                string[] parts = input[i].Split(' ', StringSplitOptions.RemoveEmptyEntries);
+                int[] coords = Array.ConvertAll(parts[0].Split('x')[1].Split("-y"), int.Parse);
+                var k = new Vector2Int(coords[0], coords[1]);
+                int ud = int.Parse(parts[2].Substring(0, parts[2].Length - 1));
+                int av = int.Parse(parts[3].Substring(0, parts[3].Length - 1));
+                nodes[k] = (ud, av);
+            }
+            return nodes;
+        }
+
         public override object Task1()
         {
-            return null;
+            Dictionary<Vector2Int, (int, int)> nodes = ParseInput();
+
+            (int, int)[] vals = new (int, int)[nodes.Count];
+            nodes.Values.CopyTo(vals, 0);
+
+            int count = 0;
+
+            for (int a = 0; a < vals.Length - 1; a++)
+            {
+                for (int b = a + 1; b < vals.Length; b++)
+                {
+                    if (vals[a].Item1 != 0 && vals[a].Item1 <= vals[b].Item2) count++;
+                    if (vals[b].Item1 != 0 && vals[b].Item1 <= vals[a].Item2) count++;
+                }
+            }
+            return count;
         }
 
         public override object Task2()
         {
-            return null;
+            char[,] grid = new char[24, 38];
+            var nodes = ParseInput();
+            int emptyCapacity = 0;
+            foreach (var kv in nodes)
+            {
+                if (kv.Value.Item1 == 0)
+                {
+                    emptyCapacity = kv.Value.Item2;
+                    break;
+                }
+            }
+
+            foreach (var kv in nodes)
+            {
+                if (kv.Value.Item1 == 0)
+                {
+                    grid[kv.Key.y, kv.Key.x] = '0';
+                }
+                else if (kv.Value.Item1 <= emptyCapacity)
+                {
+                    grid[kv.Key.y, kv.Key.x] = '·';
+                }
+                else
+                {
+                    grid[kv.Key.y, kv.Key.x] = '█';
+                }
+            }
+            grid[0, 0] = '*';
+            grid[0, 37] = 'D';
+
+            return "\nTopaz (AoC creator) said 2016.22.02 was meant to be solved without code. So if he can't be bothered I can't either.\nHere's a map. The answer is 215.\n\n" + grid.Render();
         }
     }
 }
