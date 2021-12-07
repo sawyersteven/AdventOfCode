d9f05117cfd8e9d183b3109656d143ccf70e132a
sawyersteven <sawyerstevenk@gmail.com>
2021-12-06 19:24:05 -0500

    2021 Day 06
    Kind of proud of part 2. I left part 1 as a slow brute force and part 2 is the smart way to handle since the total number of fish is irrelevant.

--- CSharp/2021/06.cs
+++ CSharp/2021/06.cs
@@ -1,17 +1,70 @@
+using System;
+using System.Collections.Generic;
 using AdventOfCode;
 
 namespace Advent2021
 {
     public class Challenge06 : Challenge
     {
-        public override object Task1()
+        private List<int> lanternFish;
+        public override void ParseInput()
         {
-            return null;
+            string[] parts = rawInput.Split(',');
+            lanternFish = new List<int>(parts.Length);
+            foreach (string s in parts) lanternFish.Add(int.Parse(s));
         }
 
+        public override object Task1()
+        {
+            List<int> lf = new List<int>(lanternFish);
+            for (int day = 0; day < 80; day++)
+            {
+                int count = lf.Count;
+                for (int i = 0; i < count; i++)
+                {
+                    if (lf[i] == 0)
+                    {
+                        lf.Add(8);
+                        lf[i] = 6;
+                    }
+                    else
+                    {
+                        lf[i]--;
+                    }
+                }
+            }
+
+            return lf.Count;
+
+        }
+
+        // obviously brute forcing this isn't going to work, and I'm kind of proud of this solution
         public override object Task2()
         {
-            return null;
+            long[] counts = new long[10];
+
+            foreach (int i in lanternFish)
+            {
+                counts[i]++;
+            }
+
+            for (int day = 0; day < 256; day++)
+            {
+                counts[9] = counts[0];
+                counts[7] += counts[0];
+                for (int i = 0; i < counts.Length - 1; i++)
+                {
+                    counts[i] = counts[i + 1];
+                }
+            }
+
+            long sum = 0;
+            for (int i = 0; i < counts.Length - 1; i++)
+            {
+                sum += counts[i];
+            }
+
+            return sum;
         }
     }
 }
