ffae42fd22bd832285a86ca47151d082868e3256
sawyersteven <sawyerstevenk@gmail.com>
2022-04-08 23:08:47 -0400

    Rust 2015 12 and 13

--- Rust/2015/src/day12.rs
+++ Rust/2015/src/day12.rs
@@ -1,5 +1,5 @@
 use crate::Base;
-use std::fmt::Display;
+use std::{fmt::Display, ops::Add};
 
 pub struct Day12 {
     pub input: String,
@@ -7,7 +7,23 @@ pub struct Day12 {
 
 impl Day12 {
     pub fn new() -> Day12 {
-        return Day12 { input: String::from("") };
+        return Day12 {
+            input: String::from(""),
+        };
+    }
+
+    fn count_nums(input: &String) -> isize {
+        let mut total: isize = 0;
+        let parts = input
+            .split([':', ',', '}', '[', ']'])
+            .collect::<Vec<&str>>();
+        for part in parts {
+            match part.parse::<isize>() {
+                Ok(d) => total += d,
+                Err(_) => {}
+            }
+        }
+        return total;
     }
 }
 
@@ -17,10 +33,79 @@ impl Base for Day12 {
     }
 
     fn part1(&mut self) -> Box<dyn Display> {
-        return Box::new("-");
+        return Box::new(Day12::count_nums(&self.input));
     }
 
     fn part2(&mut self) -> Box<dyn Display> {
-        return Box::new("-");
+        let mut remove_ranges: Vec<(usize, usize)> = Vec::new();
+
+        let chars: Vec<char> = self.input.chars().collect();
+        let mut i = self.input.len();
+        while i > 1 {
+            i -= 1;
+            if chars[i] != ':' {
+                continue;
+            }
+
+            if chars.get(i..i + 6).unwrap() != [':', '"', 'r', 'e', 'd', '"'] {
+                continue;
+            }
+
+            let mut start = i;
+            let mut end = i;
+
+            let mut depth = 1;
+            while start > 0 {
+                start -= 1;
+                depth += match chars[start] {
+                    '}' => 1,
+                    '{' => -1,
+                    _ => 0,
+                };
+                if depth == 0 {
+                    break;
+                }
+            }
+
+            depth = 1;
+            while end < chars.len() {
+                end += 1;
+                depth += match chars[end] {
+                    '}' => -1,
+                    '{' => 1,
+                    _ => 0,
+                };
+                if depth == 0 {
+                    break;
+                }
+            }
+            remove_ranges.push((start, end));
+            i = start;
+        }
+
+        remove_ranges.reverse();
+
+        let mut i = 0;
+        while i < remove_ranges.len() {
+            let mut j = i;
+            while j < remove_ranges.len() {
+                if remove_ranges[j].1 < remove_ranges[i].1 {
+                    remove_ranges.remove(j);
+                    j -= 1;
+                }
+                j += 1;
+            }
+            i += 1;
+        }
+
+        let mut combined = String::new();
+        let mut ind = 0;
+        for (start, end) in remove_ranges {
+            combined = combined.add(self.input.get(ind..start).unwrap());
+            ind = end + 1;
+        }
+        combined = combined.add(self.input.get(ind..).unwrap());
+
+        return Box::new(Day12::count_nums(&combined));
     }
 }
--- Rust/2015/src/day13.rs
+++ Rust/2015/src/day13.rs
@@ -1,26 +1,89 @@
+use itertools::Itertools;
+
 use crate::Base;
-use std::fmt::Display;
+use std::{
+    collections::{HashMap, HashSet},
+    fmt::Display,
+};
 
 pub struct Day13 {
-    pub input: String,
+    people: HashSet<String>,
+    map: HashMap<String, HashMap<String, isize>>,
 }
 
 impl Day13 {
     pub fn new() -> Day13 {
-        return Day13 { input: String::from("") };
+        return Day13 {
+            people: HashSet::new(),
+            map: HashMap::new(),
+        };
+    }
+
+    fn find_best(&self) -> isize {
+        let mut best: isize = 0;
+        for arrangement in self.people.iter().permutations(self.people.len()) {
+            let mut total: isize = 0;
+            let l = arrangement.len();
+
+            // adds one of indexes 0 and 1, -1 and -2, and two of all others
+            for i in 0..arrangement.len() - 2 {
+                total += self.map[arrangement[i + 1]][arrangement[i]];
+                total += self.map[arrangement[i + 1]][arrangement[i + 2]];
+            }
+
+            total += self.map[arrangement[0]][arrangement[1]];
+            total += self.map[arrangement[0]][arrangement[l - 1]];
+
+            total += self.map[arrangement[l - 1]][arrangement[0]];
+            total += self.map[arrangement[l - 1]][arrangement[l - 2]];
+
+            best = std::cmp::max(best, total);
+        }
+
+        return best;
     }
 }
 
 impl Base for Day13 {
     fn parse_input(&mut self, raw_input: String) {
-        self.input = raw_input;
+        for line in raw_input.split('\n') {
+            let parts = line
+                .split(' ')
+                .map(|x| x.to_string())
+                .collect::<Vec<String>>();
+
+            let val: isize = parts[3].parse::<isize>().unwrap()
+                * (if parts[2].starts_with('g') { 1 } else { -1 });
+
+            let key = parts[parts.len() - 1].trim_end_matches('.').to_string();
+            // This is apparently the onyl reasonable way to assign a k:v to a nested hashmap...
+            self.map
+                .entry(parts[0].to_owned())
+                .or_default()
+                .insert(key, val);
+
+            self.people.insert(parts[0].clone());
+        }
     }
 
     fn part1(&mut self) -> Box<dyn Display> {
-        return Box::new("-");
+        return Box::new(self.find_best());
     }
 
     fn part2(&mut self) -> Box<dyn Display> {
-        return Box::new("-");
+        for person in &self.people {
+            self.map
+                .entry("me".to_string())
+                .or_default()
+                .insert(person.to_owned(), 0);
+
+            self.map
+                .entry(person.to_owned())
+                .or_default()
+                .insert("me".to_string(), 0);
+        }
+
+        self.people.insert("me".to_string());
+        return Box::new(self.find_best());
     }
 }
