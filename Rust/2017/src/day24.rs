8eff188520357a8a6589a8bd739bfddee507fd08
sawyersteven <40702009+sawyersteven@users.noreply.github.com>
2024-05-25 21:28:38 -0400

    2017 24 and 25

--- Rust/2017/src/day24.rs
+++ Rust/2017/src/day24.rs
@@ -1,26 +1,97 @@
 use crate::Base;
-use std::fmt::Display;
+use std::{
+    collections::{HashSet, VecDeque},
+    fmt::Display,
+};
 
 pub struct Day24 {
-    pub input: String,
+    nodes: Vec<Node>,
 }
 
 impl Day24 {
     pub fn new() -> Day24 {
-        return Day24 { input: String::from("") };
+        return Day24 { nodes: Vec::new() };
     }
 }
 
 impl Base for Day24 {
     fn parse_input(&mut self, raw_input: String) {
-        self.input = raw_input;
+        for line in raw_input.split('\n') {
+            let ab: Vec<isize> = line.split('/').map(|x| x.parse().unwrap()).collect();
+            self.nodes.push(Node::new(ab[0], ab[1]));
+        }
     }
 
     fn part1(&self) -> Box<dyn Display> {
-        return Box::new("-");
+        let mut best_str = 0;
+        let mut longest = 0;
+        let mut strongest_longest_p2 = 0;
+
+        let mut stack = VecDeque::<Node>::new();
+        stack.push_back(Node::new(0, 0));
+
+        while stack.len() > 0 {
+            let current = stack.pop_back().unwrap();
+
+            best_str = best_str.max(current.strength);
+            if current.used_ids.len() > longest {
+                longest = current.used_ids.len();
+                strongest_longest_p2 = current.strength;
+            } else if current.used_ids.len() == longest {
+                strongest_longest_p2 = strongest_longest_p2.max(current.strength);
+            }
+
+            let connection_value = if current.reversed { current.a } else { current.b };
+
+            for node in &self.nodes {
+                if current.used_ids.contains(&node.id) || current.id == node.id {
+                    continue;
+                }
+
+                if node.a == connection_value {
+                    let mut next = Node::new(node.a, node.b);
+                    next.used_ids = next.used_ids.union(&current.used_ids).map(|x| *x).collect();
+                    next.used_ids.insert(current.id);
+                    next.strength = current.strength + next.a + next.b;
+                    stack.push_back(next);
+                } else if node.b == connection_value {
+                    let mut next = Node::new(node.a, node.b);
+                    next.reversed = true;
+                    next.used_ids = next.used_ids.union(&current.used_ids).map(|x| *x).collect();
+                    next.used_ids.insert(current.id);
+                    next.strength = current.strength + next.a + next.b;
+                    stack.push_back(next);
+                }
+            }
+        }
+
+        println!("Part 2: {}", strongest_longest_p2);
+        return Box::new(best_str);
     }
 
     fn part2(&self) -> Box<dyn Display> {
-        return Box::new("-");
+        return Box::new('^');
+    }
+}
+
+struct Node {
+    a: isize,
+    b: isize,
+    reversed: bool,
+    used_ids: HashSet<isize>,
+    strength: isize,
+    id: isize,
+}
+
+impl Node {
+    pub fn new(a: isize, b: isize) -> Self {
+        return Node {
+            a,
+            b,
+            reversed: false,
+            used_ids: HashSet::new(),
+            strength: 0,
+            id: a + (b << 16),
+        };
     }
 }
--- Rust/2017/src/day25.rs
+++ Rust/2017/src/day25.rs
@@ -1,26 +1,108 @@
 use crate::Base;
-use std::fmt::Display;
+use std::{
+    collections::{HashMap, VecDeque},
+    fmt::Display,
+};
 
 pub struct Day25 {
-    pub input: String,
+    starts: (isize, isize),
+    rules: HashMap<isize, [[isize; 3]; 2]>,
 }
 
 impl Day25 {
     pub fn new() -> Day25 {
-        return Day25 { input: String::from("") };
+        return Day25 {
+            starts: (0, 0),
+            rules: HashMap::new(),
+        };
     }
 }
 
 impl Base for Day25 {
     fn parse_input(&mut self, raw_input: String) {
-        self.input = raw_input;
+        let lines: Vec<&str> = raw_input.split('\n').collect();
+
+        self.starts.0 = (lines[0].bytes().nth_back(1).unwrap() - b'A') as isize;
+        self.starts.1 = lines[1].split(' ').nth_back(1).unwrap().parse().unwrap();
+
+        let mut i = 0;
+        while i < lines.len() {
+            if lines[i].is_empty() || lines[i].as_bytes()[0] != b'I' {
+                i += 1;
+                continue;
+            }
+
+            let state = (lines[i].bytes().nth_back(1).unwrap() - b'A') as isize;
+            i += 1;
+            let mut commands = [[0; 3]; 2];
+
+            while i < lines.len() {
+                if lines[i].is_empty() {
+                    i += 1;
+                    break;
+                }
+
+                let rule_num = lines[i].chars().nth_back(1).unwrap().to_digit(10).unwrap() as usize;
+                let write_val = if lines[i + 1].chars().nth_back(1).unwrap() == '0' {
+                    0
+                } else {
+                    1
+                };
+
+                let move_dir = if lines[i + 2].chars().nth_back(2).unwrap() == 'f' {
+                    -1
+                } else {
+                    1
+                };
+                let end_state = (lines[i + 3].bytes().nth_back(1).unwrap() - b'A') as isize;
+
+                commands[rule_num] = [write_val, move_dir, end_state];
+                i += 4;
+            }
+            i += 0;
+            self.rules.insert(state, commands);
+        }
     }
 
+    // The linkedlist::cursor feature is not available in stable rust as
+    // of now, so this runs slower that it should because it relies
+    // on a vecdequeue
     fn part1(&self) -> Box<dyn Display> {
-        return Box::new("-");
+        let mut state = self.starts.0;
+        let steps = self.starts.1;
+
+        let mut tape = VecDeque::<isize>::new();
+        tape.push_front(0);
+        let mut cursor_ind = 0;
+
+        for _ in 0..steps {
+            let commands = self.rules[&state][tape[cursor_ind] as usize];
+            tape[cursor_ind] = commands[0];
+
+            if commands[1] == 1 {
+                cursor_ind += 1;
+                if cursor_ind == tape.len() {
+                    tape.push_back(0);
+                }
+            } else {
+                if cursor_ind == 0 {
+                    tape.push_front(0);
+                } else {
+                    cursor_ind -= 1;
+                }
+            }
+            state = commands[2];
+        }
+
+        let mut checksum = 0;
+        for i in tape {
+            checksum += i;
+        }
+
+        return Box::new(checksum);
     }
 
     fn part2(&self) -> Box<dyn Display> {
-        return Box::new("-");
+        return Box::new("*");
     }
 }
