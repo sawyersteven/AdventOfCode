61287fbca76fe21d2fd06db59ac570b644651d51
sawyersteven <40702009+sawyersteven@users.noreply.github.com>
2024-06-26 20:01:50 -0400

    2023 14,16

--- Rust/2023/src/day14.rs
+++ Rust/2023/src/day14.rs
@@ -1,28 +1,133 @@
+use shared::{grid2d::Grid2D, utils};
+
 use crate::Base;
 use std::fmt::Display;
 
+const ROCK: u8 = b'O';
+const GROUND: u8 = b'.';
+
 pub struct Day14 {
-    pub input: String,
+    grid: Grid2D<u8>,
 }
 
 impl Day14 {
     pub fn new() -> Day14 {
         return Day14 {
-            input: String::from(""),
+            grid: Grid2D::new(0, 0),
         };
     }
 }
 
 impl Base for Day14 {
     fn parse_input(&mut self, raw_input: String) {
-        self.input = raw_input;
+        self.grid = Grid2D::<u8>::from_string(&raw_input);
     }
 
     fn part1(&mut self) -> Box<dyn Display> {
-        return Box::new("-");
+        let mut grid = self.grid.clone();
+
+        tilt_north(&mut grid);
+
+        return Box::new(north_load(&grid));
     }
 
     fn part2(&mut self) -> Box<dyn Display> {
-        return Box::new("-");
+        const OFFSET: usize = 150;
+
+        let mut grid = self.grid.clone();
+
+        // Tweak these params for maximum speed
+        match utils::find_pattern(OFFSET, 72, 72, 2, &mut || {
+            tilt_nwse(&mut grid);
+            return north_load(&grid);
+        }) {
+            None => return Box::new("Could not find stable pattern"),
+            Some(p) => {
+                println!("{}", p.len());
+                let r = (1_000_000_000 - OFFSET - 1) % p.len();
+                return Box::new(p[r]);
+            }
+        }
+    }
+}
+
+fn north_load(grid: &Grid2D<u8>) -> usize {
+    let mut total = 0;
+    let sz = grid.size();
+    for y in 0..sz.y {
+        for b in grid.iter_row(y) {
+            if *b == ROCK {
+                total += sz.y - y;
+            }
+        }
+    }
+    return total;
+}
+
+fn tilt_north(grid: &mut Grid2D<u8>) {
+    let sz = grid.size();
+    for y in 1..sz.y {
+        for x in 0..sz.x {
+            if *grid.get(x, y) == ROCK {
+                let mut move_to_y = y;
+                while move_to_y > 0 && *grid.get(x, move_to_y - 1) == GROUND {
+                    move_to_y -= 1;
+                }
+                grid.set(x, y, GROUND);
+                grid.set(x, move_to_y, ROCK);
+            }
+        }
+    }
+}
+
+fn tilt_nwse(grid: &mut Grid2D<u8>) {
+    let sz = grid.size();
+    for y in 1..sz.y {
+        for x in 0..sz.x {
+            if *grid.get(x, y) == ROCK {
+                let mut move_to_y = y;
+                while move_to_y > 0 && *grid.get(x, move_to_y - 1) == GROUND {
+                    move_to_y -= 1;
+                }
+                grid.set(x, y, GROUND);
+                grid.set(x, move_to_y, ROCK);
+            }
+        }
+    }
+    for x in 1..sz.x {
+        for y in 0..sz.y {
+            if *grid.get(x, y) == ROCK {
+                let mut move_to_x = x;
+                while move_to_x > 0 && *grid.get(move_to_x - 1, y) == GROUND {
+                    move_to_x -= 1;
+                }
+                grid.set(x, y, GROUND);
+                grid.set(move_to_x, y, ROCK);
+            }
+        }
+    }
+    for y in (0..(sz.y - 1)).rev() {
+        for x in 0..sz.x {
+            if *grid.get(x, y) == ROCK {
+                let mut move_to_y = y;
+                while move_to_y < sz.y - 1 && *grid.get(x, move_to_y + 1) == GROUND {
+                    move_to_y += 1;
+                }
+                grid.set(x, y, GROUND);
+                grid.set(x, move_to_y, ROCK);
+            }
+        }
+    }
+    for x in (0..(sz.x - 1)).rev() {
+        for y in 0..sz.y {
+            if *grid.get(x, y) == ROCK {
+                let mut move_to_x = x;
+                while move_to_x < sz.x - 1 && *grid.get(move_to_x + 1, y) == GROUND {
+                    move_to_x += 1;
+                }
+                grid.set(x, y, GROUND);
+                grid.set(move_to_x, y, ROCK);
+            }
+        }
     }
 }
--- Rust/2023/src/day16.rs
+++ Rust/2023/src/day16.rs
@@ -1,26 +1,157 @@
+use shared::{grid2d::Grid2D, v2i::Vector2Int};
+
 use crate::Base;
-use std::fmt::Display;
+use std::{
+    collections::{HashSet, VecDeque},
+    fmt::Display,
+};
 
 pub struct Day16 {
-    pub input: String,
+    grid: Grid2D<u8>,
 }
 
 impl Day16 {
     pub fn new() -> Day16 {
-        return Day16 { input: String::from("") };
+        return Day16 {
+            grid: Grid2D::new(0, 0),
+        };
+    }
+
+    fn sim_from_location(&self, start: LightBeam) -> usize {
+        let mut visited = HashSet::new();
+        let mut q = VecDeque::new();
+
+        q.push_back(start);
+
+        let grid_max: Vector2Int = self.grid.size().to_vector2() - Vector2Int::ONE;
+        while q.len() > 0 {
+            let mut current = q.pop_back().unwrap();
+            loop {
+                if !current.pos.in_range(&Vector2Int::ZERO, &grid_max) {
+                    break;
+                }
+                if visited.contains(&current) {
+                    break;
+                }
+                visited.insert(current.clone());
+
+                // select next position and enqueue a split if neccessary
+                match self.grid[current.pos] {
+                    b'-' => {
+                        if ![Vector2Int::RIGHT, Vector2Int::LEFT].contains(&current.dir) {
+                            q.push_back(LightBeam {
+                                pos: current.pos,
+                                dir: Vector2Int::LEFT,
+                            });
+
+                            current.dir = Vector2Int::RIGHT;
+                        }
+                    }
+                    b'|' => {
+                        if ![Vector2Int::UP, Vector2Int::DOWN].contains(&current.dir) {
+                            q.push_back(LightBeam {
+                                pos: current.pos,
+                                dir: Vector2Int::UP,
+                            });
+                            current.dir = Vector2Int::DOWN;
+                        }
+                    }
+                    b'\\' => {
+                        current.dir = match current.dir {
+                            Vector2Int::UP => Vector2Int::RIGHT,
+                            Vector2Int::RIGHT => Vector2Int::UP,
+                            Vector2Int::DOWN => Vector2Int::LEFT,
+                            Vector2Int::LEFT => Vector2Int::DOWN,
+                            _ => panic!(),
+                        };
+                    }
+                    b'/' => {
+                        current.dir = match current.dir {
+                            Vector2Int::UP => Vector2Int::LEFT,
+                            Vector2Int::RIGHT => Vector2Int::DOWN,
+                            Vector2Int::DOWN => Vector2Int::RIGHT,
+                            Vector2Int::LEFT => Vector2Int::UP,
+                            _ => panic!(),
+                        };
+                    }
+                    _ => {}
+                }
+                current.pos += current.dir;
+            }
+        }
+        let mut energized = HashSet::new();
+        for lb in visited {
+            energized.insert(lb.pos);
+        }
+        return energized.len();
     }
 }
 
 impl Base for Day16 {
     fn parse_input(&mut self, raw_input: String) {
-        self.input = raw_input;
+        self.grid = Grid2D::<u8>::from_string(&raw_input);
     }
 
     fn part1(&mut self) -> Box<dyn Display> {
-        return Box::new("-");
+        let start = LightBeam {
+            pos: Vector2Int { x: 0, y: 0 },
+            dir: Vector2Int::RIGHT,
+        };
+
+        let nrg = self.sim_from_location(start);
+        return Box::new(nrg);
     }
 
+    /* This could be optimized with some path memoization, but at 350ms its ok
+    enough. A thought is that memoizing certain mirrors might help. Eg if a beam
+    hits a mirror from a certain direction it will always result in the same set
+    of energized tiles which can be merged with the visited set without following
+    their path
+    */
     fn part2(&mut self) -> Box<dyn Display> {
-        return Box::new("-");
+        let mut best: usize;
+        let sz = self.grid.size();
+        best = self.sim_from_location(LightBeam {
+            pos: Vector2Int { x: 3, y: 0 },
+            dir: Vector2Int::UP,
+        });
+        // top/bottom edges
+        for x in 0..sz.x {
+            best = best.max(self.sim_from_location(LightBeam {
+                pos: Vector2Int { x: x as isize, y: 0 },
+                dir: Vector2Int::UP,
+            }));
+
+            best = best.max(self.sim_from_location(LightBeam {
+                pos: Vector2Int {
+                    x: x as isize,
+                    y: sz.y as isize,
+                },
+                dir: Vector2Int::DOWN,
+            }));
+        }
+
+        // left/right edges
+        for y in 0..sz.y {
+            best = best.max(self.sim_from_location(LightBeam {
+                pos: Vector2Int { x: 0, y: y as isize },
+                dir: Vector2Int::RIGHT,
+            }));
+
+            best = best.max(self.sim_from_location(LightBeam {
+                pos: Vector2Int {
+                    x: sz.x as isize,
+                    y: y as isize,
+                },
+                dir: Vector2Int::LEFT,
+            }));
+        }
+        return Box::new(best);
     }
+}
+
+#[derive(Clone, Eq, PartialEq, Hash)]
+struct LightBeam {
+    pos: Vector2Int,
+    dir: Vector2Int,
 }
